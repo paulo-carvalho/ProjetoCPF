ffb1962692ee3d02ae665a94fb480356e5a42bfb
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2016-04-06 00:42:13 -0300

    Model completo

--- ProjetoCPF/src/org/cpf/model/CPF.java
+++ ProjetoCPF/src/org/cpf/model/CPF.java
@@ -1,5 +1,37 @@
 package org.cpf.model;
 
 public class CPF {
+	private String entrada;
+	private Integer[] entradaNumerica;
 
+	public CPF(String entrada, Integer[] entradaNumerica) {
+		super();
+		this.entrada = entrada;
+		this.entradaNumerica = entradaNumerica;
+	}
+	
+	public CPF() {
+		super();
+		this.entrada = "00000000000";
+		this.entradaNumerica = new Integer[11];
+	}
+	
+	public String getEntrada() {
+		return entrada;
+	}
+	public void setEntrada(String entrada) {
+		this.entrada = entrada;
+	}
+	public Integer[] getEntradaNumerica() {
+		return entradaNumerica;
+	}
+	public void setEntradaNumerica(Integer[] entradaNumerica) {
+		this.entradaNumerica = entradaNumerica;
+	}
+
+	@Override
+	public String toString() {
+		return "CPF [entrada=" + entrada + "]";
+	}
+		
 }
--- ProjetoCPF/src/org/cpf/view/CPFView.java
+++ ProjetoCPF/src/org/cpf/view/CPFView.java
@@ -1,10 +1,24 @@
 package org.cpf.view;
 
+import java.util.Scanner;
+
 public class CPFView {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String bufferCPF;
+		Integer opcaoMenu;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("MENU --- CPF" + "\n" +
+				"1 - Ler nove dígitos, gerar os dígitos verificadores e escrever o CPF completo." + "\n" +
+				"2 - Ler um CPF de 11 dígitos e verificar e informar se o CPF está correto ou não." + "\n" +
+				"0 - Encerrar o programa." + "\n" +
+				"----------");
+		
+		System.out.println("Opção: ");
+		opcaoMenu = ler.nextInt();
+		
+		
 	}
 
 }
