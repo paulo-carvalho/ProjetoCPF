6fc22e3995c9a7bef734a8e7994a27c749fc1822
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2016-04-09 22:43:28 -0300

    Usando pacote Swing

--- ProjetoCPF/src/org/cpf/view/CPFView.java
+++ ProjetoCPF/src/org/cpf/view/CPFView.java
@@ -1,6 +1,6 @@
 package org.cpf.view;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 import org.cpf.controller.CPFController;
 
@@ -9,44 +9,42 @@ public class CPFView {
 	public static void main(String[] args) {
 		String bufferCPF;
 		Integer opcaoMenu;
-		Scanner ler = new Scanner(System.in);
 		
 		CPFController cpfController = new CPFController();
 		
 		while(true) {
-			System.out.println("\n" + "MENU --- CPF" + "\n" +
+			try {
+				opcaoMenu = Integer.valueOf(JOptionPane.showInputDialog(null, "\n" + "MENU --- CPF" + "\n" +
 					"1 - Ler nove dígitos, gerar os dígitos verificadores e escrever o CPF completo." + "\n" +
 					"2 - Ler um CPF de 11 dígitos e verificar e informar se o CPF está correto ou não." + "\n" +
 					"0 - Encerrar o programa." + "\n" +
-					"----------");
-			
-			System.out.print("Opção: ");
-			opcaoMenu = ler.nextInt();
+					"----------" + "\n" + 
+					"Opção: ", "MENU - Sistema CPF", JOptionPane.INFORMATION_MESSAGE));
+				
+			// quando clica em cancel, opcaoMenu = null
+			} catch (NumberFormatException e) {
+				opcaoMenu = 0;
+			}
 			
 			switch(opcaoMenu) {
 			case 1:
-				System.out.println("\n" + "Gerar CPF" + "\n");
-				System.out.println("Informe os 9 dígitos: ");
-				bufferCPF = ler.next();
+				bufferCPF = JOptionPane.showInputDialog(null, "Informe os 9 dígitos: ", "Criar CPF - Sistema CPF", JOptionPane.INFORMATION_MESSAGE);
 				
-				System.out.println(cpfController.criarCPF(bufferCPF));
+				JOptionPane.showMessageDialog(null, cpfController.criarCPF(bufferCPF), "Criar CPF - Sistema CPF", JOptionPane.INFORMATION_MESSAGE);
 				break;
 				
 			case 2:
-				System.out.println("\n" + "Checar CPF" + "\n");
-				System.out.print("Informe o CPF (apenas algarismos): ");
-				bufferCPF = ler.next();
+				bufferCPF = JOptionPane.showInputDialog(null, "Informe o CPF (apenas algarismos): ", "Checar CPF - Sistema CPF", JOptionPane.INFORMATION_MESSAGE);
 				
-				System.out.println(cpfController.checarCPF(bufferCPF));
+				JOptionPane.showMessageDialog(null, cpfController.checarCPF(bufferCPF), "Checar CPF - Sistema CPF", JOptionPane.INFORMATION_MESSAGE);
 				break;
 				
 			case 0:
-				System.out.println("Encerrando programa.");
-				ler.close();
+				JOptionPane.showMessageDialog(null, "Encerrando programa.", "Sistema CPF", JOptionPane.INFORMATION_MESSAGE);
 				return;
 				
 			default:
-				System.out.println("Opção inválida. Escolha uma das opções abaixo." + "\n");
+				JOptionPane.showMessageDialog(null, "Opção inválida. Escolha uma das opções válidas.", "Sistema CPF", JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
 	}
